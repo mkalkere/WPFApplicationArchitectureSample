74962a9dbba815703a379202c65efa713d7024ac
Kalkere, Mallikarjuna <mallikarjuna.kalkere@fmglobal.com>
2019-06-07 16:25:37 -0400

    Add project files.

---
+++ WPF.Sample.ViewModelLayer/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Common.Library;
+
+namespace WPF.Sample.ViewModelLayer
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    #region Private Variables
+    private string _LoginMenuHeader = "Login";
+    private string _StatusMessage;
+    #endregion
+
+    #region Public Properties
+    public string LoginMenuHeader
+    {
+      get { return _LoginMenuHeader; }
+      set {
+        _LoginMenuHeader = value;
+        RaisePropertyChanged("LoginMenuHeader");
+      }
+    }
+
+    public string StatusMessage
+    {
+      get { return _StatusMessage; }
+      set {
+        _StatusMessage = value;
+        RaisePropertyChanged("StatusMessage");
+      }
+    }
+    #endregion
+  }
+}
